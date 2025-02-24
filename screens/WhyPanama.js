400fbbf2deb367c3af493077fc6e9aaacbb6649c
riccovi <visserricco@gmail.com>
2025-02-24 17:36:31 +0200

    Styled 'Why Panama' page

--- screens/WhyPanama.js
+++ screens/WhyPanama.js
@@ -1,20 +1,22 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity} from 'react-native';
 import { useState, useEffect } from 'react';
+import { DateTime } from "luxon";
+import { AntDesign } from '@expo/vector-icons';
 import * as Location from 'expo-location';
+import prosAndConsData from '../data/prosAndCons.json'; 
 import BottomBar from '../components/BottomBar';
 
 function WhyPanama({}){
   const [errorMsg, setErrorMsg] = useState(null);
   const [userWeatherData, setUserWeatherData] = useState(null);
   const [panamaWeatherData, setPanamaWeatherData] = useState(null);
+  const [prosConsIndex, setProsConsIndex] = useState(0);
 
   useEffect(() => {
     (async () => {
       // Fetch Panama weather. Panama City coordinates - 8.9824° N, 79.5199° W
       try {
-        const panamaResponse = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=8.9824&lon=-79.5199&appid=d2db070e561c26a48c6d8013380bd596&units=metric`
-        );
+        const panamaResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=8.9824&lon=-79.5199&appid=d2db070e561c26a48c6d8013380bd596&units=metric`);
         const panamaJson = await panamaResponse.json();
         setPanamaWeatherData(panamaJson);
       } catch (error) {
@@ -30,9 +32,7 @@ function WhyPanama({}){
 
       try {
         let userLocation = await Location.getCurrentPositionAsync({});
-        const userResponse = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${userLocation.coords.latitude}&lon=${userLocation.coords.longitude}&appid=d2db070e561c26a48c6d8013380bd596&units=metric`
-        );
+        const userResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${userLocation.coords.latitude}&lon=${userLocation.coords.longitude}&appid=d2db070e561c26a48c6d8013380bd596&units=metric`);
         const userJson = await userResponse.json();
         setUserWeatherData(userJson);
       } catch (error) {
@@ -43,8 +43,8 @@ function WhyPanama({}){
   }, []);
 
   // Calculate current Panama time
-  const panamaTime = new Date(new Date().toLocaleString("en-US", { timeZone: "America/Panama" }));
-  const panamaHour = panamaTime.getHours();
+  const panamaTime = DateTime.now().setZone("America/Panama");
+  const panamaHour = panamaTime.hour;
   // Check if Panama is day time (between 6am and 6pm)
   const isDaytime = panamaHour >= 6 && panamaHour < 18; 
 
@@ -58,9 +58,9 @@ function WhyPanama({}){
     timeDiffText = `You are ${diffInHours} hours behind Panama time.`; // hours (plural)
   } else if (diffInHours == 1) {
     timeDiffText = `You are 1 hour behind Panama time.`; // hour (singular)
-  } else if (diffInHours < 1) {
+  } else if (diffInHours < -1) {
     timeDiffText = `You are ${Math.abs(diffInHours)} hours ahead of Panama time.`; // hours (plural)
-  } else if (diffInHours == 1) {
+  } else if (diffInHours == -1) {
     timeDiffText = `You are 1 hour ahead of Panama time.`; // hour (singular)
   } else {
     timeDiffText = "Your time is the same as Panama time.";
@@ -77,11 +77,22 @@ function WhyPanama({}){
     }
   };
 
+  // Pros and cons table
+  const pros = prosAndConsData.find((item) => item.type === "Pros")?.content || [];
+  const cons = prosAndConsData.find((item) => item.type === "Cons")?.content || []; 
+  const handleNext = () => {
+    if (prosConsIndex < pros.length - 1) setProsConsIndex(prosConsIndex + 1);
+  };
+  const handlePrev = () => {
+    if (prosConsIndex > 0) setProsConsIndex(prosConsIndex - 1);
+  };
+
+  
     return (
       <ScrollView contentContainerStyle={styles.container}>
         {/* User's Weather Section */}
         <View style={styles.section}>
-          <Text style={styles.headerText}>{userWeatherData ? `Current weather in ${userWeatherData.name}` : "Your current weather"}</Text>
+          <Text style={styles.headerText}>{userWeatherData ? `${userWeatherData.name}` : "Your current weather"}</Text>
           {errorMsg ? (
             <>
               <Text style={styles.errorText}>{errorMsg}</Text>
@@ -89,41 +100,73 @@ function WhyPanama({}){
             </>
           ) : userWeatherData ? (
             <>
-              <Text style={styles.weatherText}>{userWeatherData.weather[0].description}</Text>
-              <Text style={styles.weatherText}>Temp: {userWeatherData.main.temp}°C (Min: {userWeatherData.main.temp_min}°C, Max: {userWeatherData.main.temp_max}°C)</Text>
-              <Text style={styles.weatherText}>Precipitation: {getPrecipitation(userWeatherData)}</Text>
-              <Text style={styles.weatherText}>Humidity: {userWeatherData.main.humidity}%</Text>
-              <Text style={styles.weatherText}>Wind Speed: {userWeatherData.wind.speed} m/s</Text>
+            <View style={styles.weatherContainer}>
               <Image style={styles.weatherIcon} source={{uri:`http://openweathermap.org/img/wn/${userWeatherData.weather[0].icon}@2x.png`}}/>
+              <View style={styles.weatherDetails}>
+              <Text style={styles.weatherHead}>{userWeatherData.weather[0].description.toUpperCase()}</Text>
+                <Text style={styles.weatherText}>Temp: {userWeatherData.main.temp}°C</Text>
+                <Text style={styles.weatherText}>Precipitation: {getPrecipitation(userWeatherData)}</Text>
+                <Text style={styles.weatherText}>Humidity: {userWeatherData.main.humidity}%</Text>
+                <Text style={styles.weatherText}>Wind Speed: {userWeatherData.wind.speed} m/s</Text>
+              </View>
+            </View>
             </>
           ) : (
             <Text>Loading your weather data...</Text>
           )}
         </View>
-
+        
         {/* Panama Weather Section */}
         <View style={styles.section}>
-          <Text style={styles.headerText}>{panamaWeatherData ? `Current weather in ${panamaWeatherData.name}` : "Current weather in Panama City"}</Text>
+          <Text style={styles.headerText}>Panama City</Text>
           {panamaWeatherData ? (
             <>
-              <Text style={styles.weatherText}>{panamaWeatherData.weather[0].description}</Text>
-              <Text style={styles.weatherText}> Temp: {panamaWeatherData.main.temp}°C (Min: {panamaWeatherData.main.temp_min}°C, Max: {panamaWeatherData.main.temp_max}°C)</Text>
-              <Text style={styles.weatherText}>Precipitation: {getPrecipitation(panamaWeatherData)}</Text>
-              <Text style={styles.weatherText}>Humidity: {panamaWeatherData.main.humidity}%</Text>
-              <Text style={styles.weatherText}>Wind Speed: {panamaWeatherData.wind.speed} m/s</Text>
-              <Image style={styles.weatherIcon} source={{ uri: `http://openweathermap.org/img/wn/${panamaWeatherData.weather[0].icon}@2x.png`}}/>
+              <View style={styles.weatherContainer}>
+                <Image style={styles.weatherIcon} source={{ uri: `http://openweathermap.org/img/wn/${panamaWeatherData.weather[0].icon}@2x.png`}}/>
+                <View style={styles.weatherDetails}>
+                <Text style={styles.weatherHead}>{panamaWeatherData.weather[0].description.toUpperCase()}</Text>
+                  <Text style={styles.weatherText}>Temp: {panamaWeatherData.main.temp}°C</Text>
+                  <Text style={styles.weatherText}>Precipitation: {getPrecipitation(panamaWeatherData)}</Text>
+                  <Text style={styles.weatherText}>Humidity: {panamaWeatherData.main.humidity}%</Text>
+                  <Text style={styles.weatherText}>Wind Speed: {panamaWeatherData.wind.speed} m/s</Text>
+                </View>
+              </View>
             </>
           ) : (
             <Text>Loading Panama weather data...</Text>
           )}
-          <Text style={styles.weatherText}>Current time: {panamaTime.toLocaleTimeString()}</Text>
-          <Text style={styles.timeDiffText}>{timeDiffText}</Text>
-          <Image style={styles.panamaImage} source={isDaytime ? require('../assets/panama_day.jpg') : require('../assets/panama_night.jpg')}/>
         </View>
 
-        {/* Panama Explanation */}
+        {/* Time Section */}
         <View style={styles.section}>
-          <Text style={styles.dummyText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur, nisl sit amet ultricies malesuada, sapien dolor faucibus libero, vel dapibus libero magna vel lectus. Curabitur a eros eu magna dictum interdum. Praesent nec feugiat nunc. Etiam id est vitae purus cursus aliquet. Suspendisse potenti. Cras dictum, libero in tincidunt porttitor, augue ligula dictum justo, ac vulputate justo turpis eget purus.</Text>
+          <Text style={styles.headerText}>Panama Time: {panamaTime.toFormat("hh:mm a")}</Text>
+          <Image style={styles.panamaImage} source={isDaytime ? require('../assets/panama_day.jpg') : require('../assets/panama_night.jpg')}/>
+          <Text style={styles.weatherText}>{timeDiffText}</Text>
+        </View>
+
+        {/* Pros and Cons */}
+        <View style={styles.prosContainer}>
+          <Text style={styles.prosHeader}>Pros & Cons of Panama</Text>
+          {/* Pro */}
+          <View style={[styles.prosCard, {backgroundColor: 'lightgreen'}]}>
+            <Text style={styles.prosCardTitle}>Pro</Text>
+            <Text style={styles.prosCardContent}>{pros[prosConsIndex]}</Text>
+          </View>
+          {/* Con */}
+          <View style={[styles.prosCard, {backgroundColor: 'pink'}]}>
+            <Text style={styles.prosCardTitle}>Con</Text>
+            <Text style={styles.prosCardContent}>{cons[prosConsIndex]}</Text>
+          </View>
+          {/* Navigation Arrows */}
+          <View style={styles.prosNavContainer}>
+            <TouchableOpacity onPress={handlePrev} disabled={prosConsIndex === 0}>
+              <AntDesign name="leftcircleo" size={32} color={prosConsIndex === 0 ? "gray" : "black"} />
+            </TouchableOpacity>
+            <Text style={styles.prosIndexText}>{prosConsIndex + 1} / {pros.length}</Text>
+            <TouchableOpacity onPress={handleNext} disabled={prosConsIndex === pros.length - 1}>
+              <AntDesign name="rightcircleo" size={32} color={prosConsIndex === pros.length - 1 ? "gray" : "black"} />
+            </TouchableOpacity>
+          </View>
         </View>
 
         <BottomBar />
@@ -135,10 +178,10 @@ export default WhyPanama;
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    padding: 20,
+    paddingTop: 50,
+    paddingHorizontal: 20,
+    paddingBottom: 20,
     backgroundColor: '#fff',
-    justifyContent: 'center',
   },
   section: {
     marginBottom: 20,
@@ -150,40 +193,86 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     textAlign: 'center',
   },
-  weatherText: {
-    fontSize: 16,
-    marginVertical: 4,
-    textAlign: 'center',
-  },
-  weatherIcon: {
-    width: 100,
-    height: 100,
-    marginVertical: 10,
-  },
-  timeDiffText: {
-    fontSize: 16,
-    marginVertical: 10,
-    textAlign: 'center',
-  },
-  panamaImage: {
-    width: 300,
-    height: 200,
-    marginTop: 10,
-    resizeMode: 'cover',
-  },
-  dummyText: {
-    fontSize: 16,
-    lineHeight: 24,
-    textAlign: 'center',
-  },
+
   errorText: {
     fontSize: 18,
     color: 'red',
     textAlign: 'center',
-    marginBottom: 10,
   },
   instructions: {
     fontSize: 16,
     textAlign: 'center',
   },
+
+  weatherContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'lightblue', 
+    padding: 10,
+    borderRadius: 10,
+    width: '100%',
+  },
+  weatherIcon: {
+    width: 100,
+    height: 100,
+  },
+  weatherDetails: {
+    flex: 1,
+  },
+  weatherHead: {
+    fontSize: 20,
+    marginVertical: 10,
+    textAlign: 'center',
+    fontStyle: 'italic',
+  },
+  weatherText: {
+    fontSize: 16,
+    marginVertical: 4,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
+
+  panamaImage: {
+    width: 300,
+    height: 200,
+    borderRadius: 40,
+    borderWidth: 3,
+    borderColor: 'black',
+  },
+
+  prosContainer: { 
+    padding: 20, 
+    alignItems: "center",
+    paddingBottom: 80, 
+  },
+  prosHeader: { 
+    fontSize: 24, 
+    fontWeight: "bold", 
+    marginBottom: 20 
+  },
+  prosCard: { 
+    width: "90%", 
+    padding: 15, 
+    marginBottom: 10, 
+    borderWidth: 1, 
+    borderRadius: 10, 
+    alignItems: 'center'
+  },
+  prosCardTitle: { 
+    fontSize: 18, 
+    fontWeight: "bold", 
+  },
+  prosCardContent: { 
+    fontSize: 16, 
+    marginTop: 5,
+  },
+  prosNavContainer: { 
+    flexDirection: "row", 
+    alignItems: "center", 
+    marginTop: 20 
+  },
+  prosIndexText: {
+    fontSize: 18, 
+    marginHorizontal: 20 
+  }
 });
